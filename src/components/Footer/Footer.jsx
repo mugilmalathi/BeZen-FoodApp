c2936a564a3a96c1a941b3e1c1a19fdf1e8ecdb0
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-09-28 18:19:09 +0530

    readme and screenshots added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,10 +2,34 @@ import React from 'react'
 import "./Footer.scss"
 import "../Responsive/Responsive.scss"
 
+import { BsLinkedin } from "react-icons/bs";
+import { BsGlobe } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { BsYoutube } from "react-icons/bs";
+
 const Footer = () => {
+
+  const handleLinkedIn=()=>{
+    window.open("https://www.linkedin.com/in/mugil-arasan/")
+  }
+  const handlePortfolio=()=>{
+    window.open("https://mugilarasan-portfolio.vercel.app/#")
+  }
+  const handleGitHub=()=>{
+    window.open("https://github.com/mugilmalathi")
+  }
+
   return (
     <div className='footer'>
-        Footer
+        <div>
+          <BsLinkedin className='icon' onClick={handleLinkedIn}/>
+          <BsGlobe className='icon' onClick={handlePortfolio}/>
+          <BsGithub className='icon' onClick={handleGitHub}/>
+          <BsInstagram className='icon'/>
+          <BsYoutube className='icon'/>
+        </div>
+        <div>2022 ©Copyright Reserved by Mugilarasan</div>
     </div>
   )
 }
