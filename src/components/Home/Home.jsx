0d51863e236989e2f742dd431c3510afb1e4f511
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-09-28 13:36:02 +0530

    skeleton animation added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,82 +1,104 @@
-import React, { useContext, useEffect, useState } from 'react'
-import "./Home.scss"
+import React, { useContext, useEffect, useState } from "react";
+import "./Home.scss";
 // import { FaSearch } from "react-icons/fa";
-import axios from 'axios';
-import { useNavigate } from 'react-router';
+import axios from "axios";
+import { useNavigate } from "react-router";
+import Skeleton from "react-loading-skeleton";
 
 const Home = () => {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
 
-    const[search, setSearch]=useState("")
-    const[data, setData]=useState([])
-    const navigate = useNavigate()
+  useEffect(() => {
+    axios
+      .get("https://food-recipe-details.herokuapp.com/food")
+      .then((res) => {
+        setData(res.data);
+      })
+      .then((e) => {
+        handleDelete(e);
+      });
+  }, [data]);
 
-    useEffect(()=>{
-        axios.get("https://food-recipe-details.herokuapp.com/food")
-        .then((res)=>{
-            setData(res.data)
-        })
-        .then((e)=>{
-            handleDelete(e)
-        })
-    }, [data])
+  const handleDelete = (id) => {
+    fetch(`https://food-recipe-details.herokuapp.com/food/${id}`, {
+      method: "DELETE",
+    }).then((result) => {
+      result.json();
+    });
+  };
 
-    const handleDelete = (id)=>{
-        fetch(`https://food-recipe-details.herokuapp.com/food/${id}`,{
-            method: 'DELETE'
-        }).then((result)=>{
-            result.json()
-        })
-    }
+  const handleUpdate = (id) => {
+    localStorage.setItem("foodID", JSON.stringify(id));
+  };
 
-    const handleUpdate = (id)=>{
-        // console.log(id, "checking checking cehecking ID");
-        localStorage.setItem("foodID", JSON.stringify(id))
-    }
-
-    const handleSingleFood =(event)=>{
-        console.log(event, 'checking checking checking...')
-        localStorage.setItem("singleFood", JSON.stringify(event))
-        navigate("/singleFood")
-    }
-
+  const handleSingleFood = (event) => {
+    localStorage.setItem("singleFood", JSON.stringify(event));
+    navigate("/singleFood");
+  };
 
   return (
-    <div className='home'>
-          <div className='search-main'>
-            <input
-              className="search"
-              type="text"
-              placeholder="Enter Food Recipe..."
-              onChange={(e)=>{
-                setSearch(e.target.value)
-              }}
-            />
-          </div>
-        <div className='content'>
-            {
-                data.filter((e)=>e.name.toLowerCase().includes(search))
-                .map((el)=>{
-                    return(
-                        <div className='box' onClick={()=>{
-                            handleSingleFood(el)
-                        }}>
-                            <div><img src={el.img} alt="" /></div>
-                            <div>{el.name}</div>
-                            <div>{el.ingredients}</div>
-                            <div>
-                                <button onClick={()=>{
-                                    handleUpdate(el._id)
-                                    navigate("/update")
-                                }}>Edit</button>
-                                <button onClick={()=>handleDelete(el._id)}>Delete</button>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-        </div>
+    <div className="home">
+      <div className="search-main">
+        <input
+          className="search"
+          type="text"
+          placeholder="Enter Food Recipe..."
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+      </div>
+      <div className="content">
+        {data
+          ? data
+              .filter((e) => e.name.toLowerCase().includes(search))
+              .map((el) => {
+                return (
+                  <div
+                    className="box"
+                    onClick={() => {
+                      handleSingleFood(el);
+                    }}
+                  >
+                    <div>
+                      <img src={el.img} alt="" />
+                    </div>
+                    <div>{el.name}</div>
+                    <div>{el.ingredients}</div>
+                    <div>
+                      <button
+                        onClick={() => {
+                          handleUpdate(el._id);
+                          navigate("/update");
+                        }}
+                      >
+                        Edit
+                      </button>
+                      <button onClick={() => handleDelete(el._id)}>
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                );
+              })
+          : [...Array(8)].map((data) => {
+              return (
+                <div className="box">
+                  <div>
+                    <Skeleton
+                        circle
+                    />
+                  </div>
+                  <div><Skeleton /></div>
+                  <div><Skeleton /></div>
+                </div>
+              );
+            })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
